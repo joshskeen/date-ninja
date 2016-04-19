a6309bb3827770024ea330e31b2317f7396322c6
Adrian Bolinger <adrian.bolinger@me.com>
2016-04-19 15:47:55 -0400

    Added basic test

--- date_helper.js
+++ date_helper.js
@@ -1,5 +1,26 @@
 'use strict';
+var _ = require('lodash');
+var rp = require('request-promise');
 
-function DateHelper() { }
+// constants
+var minutes = 1000*60;
+var hours = minutes*60;
+var days = hours*24;
+
+
+function DateHelper() {
+
+	// TODO: need to add a units parameter to the function
+	DateHelper.prototype.daysUntil = function(futureDate) {
+		// fill in
+
+		var now = new Date();
+		// TODO: need a switch here
+		var dateDifference = Math.round((futureDate - now)/days);
+		console.log('dateDifference = ' + dateDifference);
+		return dateDifference
+	}
+
+}
 
 module.exports = DateHelper;
--- test/test_date_helper.js
+++ test/test_date_helper.js
@@ -2,7 +2,20 @@
 var chai = require('chai');
 var expect = chai.expect;
 var DateHelper = require('../date_helper');
+chai.config.includeStack = true; // true enables stack trace
 
 describe('DateHelper', function() {
   var subject = new DateHelper();
+  var futureDate;
+  describe('#daysUntil', function() {
+  	context('with a valid date', function() {
+  		it('returns the correct date in the future', function() {
+  			futureDate = new Date('April 21, 2016'); // I want to test for tomorrow
+  			var value = subject.daysUntil(futureDate).then(function(obj) {
+  				return obj;
+  			});
+  			return expect(value).to.eq(1);
+  		});
+  	});
+  });
 });
