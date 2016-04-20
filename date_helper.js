c85849b698ad86348c55dd23721c0c36ebfed464
Adrian Bolinger <adrian.bolinger@me.com>
2016-04-19 21:17:52 -0400

    Refactored to remove redundant method, handle interaction based on positive/negative number

--- date_helper.js
+++ date_helper.js
@@ -4,39 +4,19 @@ var rp = require('request-promise');
 let moment = require('moment');
 // moment().format(); // not sure if this is needed
 
-// STOCK TUTORIAL CODE
-// function DateHelper() {
-
-// 	DateHelper.prototype.requestDaysUntil = function(futureDate) {
-// 		return this.getDaysUntil(futureDate).then(
-// 			function(response) {
-// 				console.log('success - daysUntil futureDate is ' + response);
-// 				return response.body;
-// 			}
-// 		);
-// 	};
-
-// 	// TODO: need to add a units parameter to the function
-// 	DateHelper.prototype.getDaysUntil = function(futureDate) {
-
-// 		var currentDate = moment().format("YYYY MM DD");
-// 		var daysUntil = futureDate.diff(currentDate, 'days');
-// 		console.log('daysUntil = ' + daysUntil);
-// 		return daysUntil;
-// 	};
-// }
-
 function DateHelper() {
 
 	// TODO: need to add a units parameter to the function
-	DateHelper.prototype.getDaysUntil = function(futureDate) {
+	DateHelper.prototype.getDaysFromDate = function(futureDate) {
 
 		var currentDate = moment().format("YYYY MM DD");
-		var daysUntil = futureDate.diff(currentDate, 'days');
-		console.log('daysUntil = ' + daysUntil);
-		return daysUntil;
+		var daysFromDate = futureDate.diff(currentDate, 'days');
+		console.log('daysFromDate = ' + daysFromDate);
+		// return values
+		// positive number = days until
+		// negative number = days since
+		return daysFromDate;
 	};
 }
-
 
 module.exports = DateHelper;
--- test/test_date_helper.js
+++ test/test_date_helper.js
@@ -8,20 +8,29 @@ moment().format('YYYY MM DD');
 
 describe('DateHelper', function() {
 
-	// getDaysUntil method
+	// getDaysFromDate method
 	var subject = new DateHelper();
-	var futureDate;
-	context('With a valid date', function() {
-		it('Should return 2', function() {
+	var futureDate; // for testing with getDaysFromDate function
+	var pastDate; 	// for testing with getDaysFromDate function
+
+	context('With a valid date, getDaysFromDate', function() {
+		it('should return 2', function() {
 			futureDate = moment('2016 04 21', 'YYYY MM DD'); // I want to test for tomorrow
-			expect(subject.getDaysUntil(futureDate)).to.eq(2);
+			expect(subject.getDaysFromDate(futureDate)).to.eq(2);
 		});
 	});
 
-	context('With an invalid date', function() {
+	context('With an invalid date, getDaysFromDate', function() {
 		it('Should return an error', function() {
 			futureDate = moment('90322 999 323', 'YYYY MM DD'); // I want to test for a silly value
-			expect(subject.getDaysUntil(futureDate)).to.be.NaN;
+			expect(subject.getDaysFromDate(futureDate)).to.be.NaN;
+		});
+	});
+
+	context('With a valid date, getDaysFromDate', function() {
+		it('should return -18', function() {
+			pastDate = moment('2016 04 01', 'YYYY MM DD'); // I want to test for tomorrow
+			expect(subject.getDaysFromDate(pastDate)).to.eq(-18);
 		});
 	});
 });
